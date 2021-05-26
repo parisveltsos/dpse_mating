f2508f52deeeaa4c752a1babb34d075bbbe7b10f
Paris Veltsos <parisveltsos@gmail.com>
2021-05-25 22:41:47 -0500

    lines interaction plot

--- scripts/newplot.r
+++ scripts/newplot.r
@@ -24,8 +24,24 @@ abline(coef = c(0,1), col='darkgreen')
 plot(kdata$logFC.E.MvsEE.MM, kdata$logFC.E.MMvsM.EE, xlab="virgin vs mated logFC", ylab="interaction logFC", main="virgin/mated vs interaction", cex.main=1.8, cex.lab=1.3, col='gray80')
 points(kdata$logFC.E.MvsEE.MM[kdata$FDR.E.MMvsM.EE<0.05], kdata$logFC.E.MMvsM.EE[kdata$FDR.E.MMvsM.EE<0.05], pch=20, col=4)
 points(kdata$logFC.E.MvsEE.MM[kdata$FDR.E.MvsEE.MM<0.05], kdata$logFC.E.MMvsM.EE[kdata$FDR.E.MvsEE.MM<0.05], pch=4, col=2)
-legend("topleft", inset=0.05, legend=c("E.MMvsM.EE", "E.MvsEE.MM"), pch =c(20,4), col=c(4,2) ) 
+points(kdata$logFC.E.MvsEE.MM[kdata$FDR.allEvsM<0.05], kdata$logFC.E.MMvsM.EE[kdata$FDR.allEvsM<0.05], pch=3, col=6)
+legend("topleft", inset=0.05, legend=c("E.MMvsM.EE", "E.MvsEE.MM", "all E vs M"), pch =c(20,4,3), col=c(4,2, 6) ) 
 abline(h=0, col='darkgreen')
 abline(v=0, col='darkgreen')
 
+
+
+idata <- subset(kdata, kdata$FDR.E.MMvsM.EE<0.05)
+head(idata)
+par(mar=c(5,5,4,3))
+plot(c(1,2), c(min(idata$logFC.EvsM, idata$logFC.EEvsMM), max(idata$logFC.EvsM, idata$logFC.EEvsMM)), type='n', ylab="logFC E vs M", xlab="", main='E vs M interaction virgin vs mated')
+
+for (i in 1:nrow(idata)) {
+	if (idata$logFC.EvsM[i] > idata$logFC.EEvsMM[i]) {
+		lines(c(idata$logFC.EvsM[i],idata$logFC.EEvsMM[i]), col=2)
+		} else {
+			lines(c(idata$logFC.EvsM[i],idata$logFC.EEvsMM[i]), col=4)
+		}
+	}
+legend("topright", inset=0.05, legend=c("virgin > mated", "virgin < mated"), pch =c(19,19), col=c(2,4), cex=0.8 )
 
